e3803d6a808ec63ef1af175c730a02b6e906f5a2
annahutsaliuk <hannah.htslk@gmail.com>
2025-06-08 22:02:03 +0300

    add logout

--- src/pages/AdminDashboard/AdminDashboard.jsx
+++ src/pages/AdminDashboard/AdminDashboard.jsx
@@ -1,3 +1,23 @@
+import { useDispatch } from 'react-redux';
+import { logout } from '../../redux/auth/operations';
+import { useNavigate } from 'react-router-dom';
+
 export const AdminDashboard = () => {
-  return <div>AdminDashboard</div>;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleLogout = async () => {
+    try {
+      await dispatch(logout()).unwrap();
+      navigate('/');
+    } catch (error) {
+      console.error('Logout failed:', error);
+    }
+  };
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
 };

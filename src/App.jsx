8b1d1a857949e94355d36f8524effa8690103218
annahutsaliuk <hannah.htslk@gmail.com>
2025-06-09 23:11:42 +0300

    add new routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 // import { useEffect, useState } from 'react';
 import '@/App.css';
-import '@/seedProducts';
 // import { fetchProducts } from './services/productsAPI';
 import { Route, Routes } from 'react-router-dom';
 import PrivateRoute from '@/routes/PrivateRoute';
 import { Home } from '@/pages/HomePage/Home';
 import { Login } from '@/pages/LoginPage/Login';
 import { AdminDashboard } from '@/pages/AdminDashboard/AdminDashboard';
+import { About } from '@/pages/AboutPage/About';
+import { Catalogue } from '@/pages/CataloguePage/Catalogue';
+import { Cart } from '@/pages/CartPage/Cart';
+import { Layout } from '@/components/Layout/Layout';
 
 function App() {
   // const [products, setProducts] = useState([]);
@@ -20,18 +23,23 @@ function App() {
   // }, []);
 
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route
-        path="/admin"
-        element={
-          <PrivateRoute>
-            <AdminDashboard />
-          </PrivateRoute>
-        }
-      />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/catalogue" element={<Catalogue />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/admin"
+          element={
+            <PrivateRoute>
+              <AdminDashboard />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </Layout>
   );
 }
 
